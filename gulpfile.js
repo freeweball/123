c24fcdf8c94112c7a766d9c400e7e6fb9eaad82d
Aleksandr Savostin <sav@dextra.ru>
2020-03-10 11:49:32 +0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,120 @@
+const gulp = require(`gulp`);
+const plumber = require(`gulp-plumber`);
+const sourcemap = require(`gulp-sourcemaps`);
+const sass = require(`gulp-sass`);
+const postcss = require(`gulp-postcss`);
+const autoprefixer = require(`autoprefixer`);
+const server = require(`browser-sync`).create();
+const csso = require(`gulp-csso`);
+const rename = require(`gulp-rename`);
+const imagemin = require(`gulp-imagemin`);
+const webp = require(`gulp-webp`);
+const svgstore = require(`gulp-svgstore`)
+const posthtml = require(`gulp-posthtml`);
+const include = require(`posthtml-include`);
+const del = require(`del`);
+// const rollup = require(`gulp-better-rollup`);
+// const sourcemaps = require(`gulp-sourcemaps`);
+
+gulp.task(`scripts`, () => {
+  return gulp.src(`source/js/**/*.js`)
+    .pipe(plumber())
+    .pipe(gulp.dest(`build/js/`));
+});
+
+gulp.task(`css-min`, () => {
+  return gulp.src(`source/sass/style.scss`)
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(csso())
+    .pipe(rename(`style.min.css`))
+    .pipe(sourcemap.write(`.`))
+    .pipe(gulp.dest(`build/css`))
+    .pipe(server.stream());
+});
+
+gulp.task(`css`, () => {
+  return gulp.src(`source/sass/style.scss`)
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(gulp.dest(`build/css`))
+    .pipe(server.stream());
+});
+
+gulp.task(`server`, () => {
+  server.init({
+    server: `build/`,
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch(`source/sass/**/*.{scss,sass}`, gulp.series(`css-min`, `css`));
+  gulp.watch(`source/img/icon-*.svg`, gulp.series(`sprite`, `html`, `refresh`));
+  gulp.watch(`source/*.html`, gulp.series(`html`, `refresh`));
+  gulp.watch(`source/js/**/*.js`, gulp.series(`scripts`, `refresh`));
+});
+
+gulp.task(`refresh`, (done) => {
+  server.reload();
+  done();
+});
+
+gulp.task(`images`, () => {
+  return gulp.src(`source/img/**/*.{png,jpg,svg}`)
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+
+    .pipe(gulp.dest(`source/img`));
+
+});
+
+gulp.task(`webp`, () => {
+  return gulp.src(`source/img/**/*.{png,jpg}`)
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest(`source/img`));
+});
+
+gulp.task(`sprite`, () => {
+  return gulp.src(`source/img/icon-*.svg`)
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename(`sprite.svg`))
+    .pipe(gulp.dest(`build/img/`));
+});
+
+gulp.task(`html`, () => {
+  return gulp.src(`source/*.html`)
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest(`build`));
+});
+
+gulp.task(`copy`, () => {
+  return gulp.src([
+    `source/fonts/**/*.{woff,woff2}`,
+    `source/img/**`,
+    `source/js/**/*.js`,
+    `source/*.ico`
+    ], {
+      base: `source`
+    })
+  .pipe(gulp.dest(`build`));
+});
+
+gulp.task(`clean`, () => {
+  return del(`build`);
+});
+
+gulp.task(`build`, gulp.series(`clean`, `copy`, `css-min`, `css`, `sprite`, `html`, `scripts`));
+gulp.task(`start`, gulp.series(`build`, `server`));
---
+++ source/js/slider.js
@@ -0,0 +1,117 @@
+const sliders2 = `.swiper-container-s2`;
+const sliders3 = `.swiper-container-s3`;
+
+const slideHeightSwiper = (SliderClass) => {
+  const slider = document.querySelector(SliderClass);
+  const slideList = slider.querySelectorAll(`.ed-curses__slide`);
+  let slideHeight = 0;
+
+  for (let i = 0; i < slideList.length; i++) {
+    let maxHeight = slideList[i].offsetHeight;
+    if (maxHeight > slideHeight) {
+      slideHeight = maxHeight;
+    }
+  };
+    for (let i = 0; i < slideList.length; i++) {
+      slideList[i].style.height = slideHeight + `px`;
+    };
+};
+
+var mySwiper = new Swiper('.swiper-container-s1', {
+    speed: 400,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+    nextEl: '.swiper-button-next-s1',
+    prevEl: '.swiper-button-prev-s1',
+  },
+});
+
+var mySwiper = new Swiper('.swiper-container-s2', {
+    speed: 400,
+    slidesPerView: 3,
+    spaceBetween: 42,
+    breakpoints: {
+      850: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      580: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      }
+    },
+    navigation: {
+    nextEl: '.swiper-button-next-s2',
+    prevEl: '.swiper-button-prev-s2',
+    on: {
+      init: setTimeout(() => slideHeightSwiper(sliders2), 500),
+    },
+  },
+});
+
+var mySwiper = new Swiper('.swiper-container-s3', {
+    speed: 400,
+    slidesPerView: 3,
+    spaceBetween: 42,
+    breakpoints: {
+      850: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      580: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      }
+    },
+    navigation: {
+    nextEl: '.swiper-button-next-s3',
+    prevEl: '.swiper-button-prev-s3',
+    on: {
+      init: setTimeout(() => slideHeightSwiper(sliders3), 500),
+    },
+  },
+});
+
+var mySwiper = new Swiper('.swiper-container-s4', {
+    speed: 400,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    navigation: {
+    nextEl: '.swiper-button-next-s4',
+    prevEl: '.swiper-button-prev-s4',
+  },
+});
+
+var mySwiper = new Swiper('.swiper-container-s5', {
+    speed: 400,
+    slidesPerView: 3,
+    spaceBetween: 42,
+    breakpoints: {
+      850: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      580: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      }
+    },
+    navigation: {
+    nextEl: '.swiper-button-next-s5',
+    prevEl: '.swiper-button-prev-s5',
+  },
+});
+
+var mySwiper = new Swiper('.swiper-container-s6', {
+    speed: 400,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    navigation: {
+    nextEl: '.swiper-button-next-s6',
+    prevEl: '.swiper-button-prev-s6',
+  },
+});
